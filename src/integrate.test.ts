639bb62a0f736d6c7bcbde0807916174aa4d1bf5
すえ <souhait.0614@gmail.com>
2022-07-10 00:24:01 +0900

    chore: テストの追加

--- src/integrate.test.ts
+++ src/integrate.test.ts
@@ -34,4 +34,37 @@ describe('createStore', () => {
     setSignal((prev) => prev + 1)
     expect(store.get()).toEqual(2)
   })
+  it('store and signal set', () => {
+    const str2 = "Integrated!"
+    const str3 = "Integrated!!"
+    const str4 = "Integrated!!!"
+    const results = []
+    const store = createStore('')
+    const [getSignal, setSignal] = createSignal('')
+    integrate(store, [getSignal, setSignal])
+    store.set(str)
+    results.push(getSignal())
+    store.set(str2)
+    results.push(getSignal())
+    setSignal(str3)
+    results.push(store.get())
+    setSignal(str4)
+    results.push(store.get())
+    expect(results).toEqual([str, str2, str3, str4])
+  })
+  it('store and signal update', () => {
+    const results = []
+    const store = createStore(0)
+    const [getSignal, setSignal] = createSignal(0)
+    integrate(store, [getSignal, setSignal])
+    store.update((prev) => prev + 1)
+    results.push(getSignal())
+    store.update((prev) => prev + 1)
+    results.push(getSignal())
+    setSignal((prev) => prev + 1)
+    results.push(store.get())
+    setSignal((prev) => prev + 1)
+    results.push(store.get())
+    expect(results).toEqual([1, 2, 3, 4])
+  })
 })
